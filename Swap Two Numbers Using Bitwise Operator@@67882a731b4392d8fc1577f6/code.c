18b9b0de70a4fedd5ec148e47f83ef826209168a
abhinav <abhinavkalsana@gmail.com>
2025-02-06 12:18:08 +0530

    Solution to Swap Two Numbers Using Bitwise Operator

--- Swap Two Numbers Using Bitwise Operator@@67882a731b4392d8fc1577f6/code.c
+++ Swap Two Numbers Using Bitwise Operator@@67882a731b4392d8fc1577f6/code.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 
-void printBinary(int num) {
-    int i;
-    int started = 0; // To ignore leading zeros
-
-    for (i = 31; i >= 0; i--) { // Loop through all 32 bits
-        if (num & (1 << i)) { // Check if the bit is set (1)
-            started = 1;
-            printf("1");
-        } else if (started) { // Print only after first '1' is found
-            printf("0");
-        }
-    }
-
-    if (!started) { // If num is 0, print "0"
-        printf("0");
-    }
-
-    printf("\n");
-}
-
 int main() {
-    int num;
-    scanf("%d", &num);
+    int a ,b;
+    printf("enter the value for a & b:\n");
+    scanf("%d%d",&a,&b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
 
-    printBinary(num); // Call function to print binary representation
     return 0;
 }
