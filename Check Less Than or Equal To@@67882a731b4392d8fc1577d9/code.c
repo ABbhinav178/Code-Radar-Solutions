c7833a2c25bdba693caf8568ecdf32d371a37084
abhinav <abhinavkalsana@gmail.com>
2025-02-15 22:26:52 +0530

    Solution to Check Less Than or Equal To

--- Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
+++ Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
@@ -2,23 +2,13 @@
 #include <stdbool.h>  // Include for bool, true, false
 
 int main() {
-    int num1, num2;
-    bool result;  // Boolean variable
-
-    // Input two numbers
-    scanf("%d", &num1);
-
-    
-    scanf("%d", &num2);
-
-    // Check if num1 is less than or equal to num2
-    result = (num1 <= num2);
-
-    // Print result as true or false
-    if (result) {
-        printf("True\n");
-    } else {
-        printf("False\n");
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a<=b){
+        printf("True");
+    }else{
+        printf("False");
     }
 
     return 0;
